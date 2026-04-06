d8fca0fd1b8ea449a258884db9d29f88fa3987cb
ifarrell625 <ifarrell625@gmail.com>
2026-04-05 23:41:51 -0300

    added node-based state machine

---
+++ scenes/characters/character_states/player_state_tackling.gd
@@ -0,0 +1,14 @@
+class_name PlayerStateTackling
+extends PlayerState
+
+const DURATION_TACKLE := 200
+
+var timer_start_tackle := Time.get_ticks_msec()
+
+func _enter_tree() -> void:
+	animation_player.play("tackle")
+	timer_start_tackle = Time.get_ticks_msec()
+
+func _process(_delta: float) -> void:
+	if Time.get_ticks_msec() - timer_start_tackle > DURATION_TACKLE:
+		state_transition_requested.emit(Player.State.MOVING)
