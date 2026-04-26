7ef7f3f650d6674c05758ed0794e9e356d4c1933
ifarrell625 <ifarrell625@gmail.com>
2026-04-26 18:14:31 -0300

    added shader to replace colors

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -7,6 +7,7 @@ const CONTROL_SCHEME_MAP : Dictionary ={
 	ControlScheme.P2: preload("res://assets/art/props/2p.png")
 }
 
+const COUNTRIES := ["DEFAULT", "FRANCE", "ARGENTINA", "BRAZIL", "ENGLAND", "GERMANY", "ITALY", "SPAIN", "USA"]
 const GRAVITY := 8.0
 const BALL_CONTROL_HEIGHT_MAX := 10.0
 
@@ -28,7 +29,7 @@ enum State {MOVING, TACKLING, RECOVERING, PREPPING_SHOT, SHOOTING, PASSING, HEAD
 @onready var player_sprite: Sprite2D = %PlayerSprite
 @onready var teammate_detection_area: Area2D = %TeammateDetectionArea
 
-
+var country := ""
 var current_state: PlayerState = null
 var fullname := ""
 var heading := Vector2.RIGHT
@@ -41,6 +42,7 @@ var state_factory := PlayerStateFactory.new()
 func _ready() -> void:
 	set_control_texture()
 	switch_state(State.MOVING)
+	set_shader_properties()
 
 func _process(delta: float) -> void:
 	flip_sprites()
@@ -48,7 +50,13 @@ func _process(delta: float) -> void:
 	process_gravity(delta)
 	move_and_slide()
 
-func initialize(context_position: Vector2, context_ball: Ball, context_own_goal: Goal, context_target_goal: Goal, context_player_data: PlayerResource)-> void:
+func set_shader_properties() -> void:
+	player_sprite.material.set_shader_parameter("skin_color", skin_color)
+	var country_color := COUNTRIES.find(country)
+	country_color = clampi(country_color, 0, COUNTRIES.size() - 1)
+	player_sprite.material.set_shader_parameter("team_color", country_color)
+
+func initialize(context_position: Vector2, context_ball: Ball, context_own_goal: Goal, context_target_goal: Goal, context_player_data: PlayerResource, context_country: String)-> void:
 	position = context_position
 	ball = context_ball
 	own_goal = context_own_goal
@@ -59,6 +67,7 @@ func initialize(context_position: Vector2, context_ball: Ball, context_own_goal:
 	skin_color = context_player_data.skin_color
 	fullname = context_player_data.full_name
 	heading = Vector2.LEFT if target_goal.position.x < position.x else Vector2.RIGHT
+	country = context_country
 
 func switch_state(state: State, state_data: PlayerStateData = PlayerStateData.new()) -> void:
 	if current_state != null:
