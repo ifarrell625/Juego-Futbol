c583c639d307399767fbcecd260ed15fa5c257d2
ifarrell625 <ifarrell625@gmail.com>
2026-07-01 03:22:12 -0300

    completed entire gameplay loop

--- scenes/game_manager/game_manager.gd
+++ scenes/game_manager/game_manager.gd
@@ -6,13 +6,14 @@ enum State {IN_PLAY, SCORED, RESET, KICKOFF, OVERTIME, GAMEOVER}
 
 var countries : Array[String] = ["ARGENTINA", "SPAIN"]
 var current_state : GameState = null
+var player_setup : Array[String] = ["ARGENTINA", ""]
 var score :=[0, 0]
 var state_factory := GameStateFactory.new()
 var time_left : float
 
 func _ready() -> void:
 	time_left = DURATION_GAME_SEC
-	switch_state(State.IN_PLAY)
+	switch_state(State.RESET)
 
 func switch_state(state: State, data: GameStateData = GameStateData.new()) -> void:
 	if current_state != null:
@@ -22,3 +23,9 @@ func switch_state(state: State, data: GameStateData = GameStateData.new()) -> vo
 	current_state.state_transition_requested.connect(switch_state.bind())
 	current_state.name = "GameStateMachine: " + str(state)
 	call_deferred("add_child", current_state)
+
+func is_coop() -> bool:
+	return player_setup[0] == player_setup[1]
+
+func is_single_player() -> bool:
+	return player_setup[1].is_empty()
--- scenes/game_manager/game_states/game_state_scored.gd
+++ scenes/game_manager/game_states/game_state_scored.gd
@@ -1,7 +1,7 @@
 class_name GameStateScored
 extends GameState
 
-const DURATION_CELEBRATION := 3000
+const DURATION_CELEBRATION := 2500
 
 var time_since_celebration = Time.get_ticks_msec()
 
@@ -10,6 +10,6 @@ func _enter_tree() -> void:
 	manager.score[index_country_scoring] += 1
 	time_since_celebration = Time.get_ticks_msec()
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Time.get_ticks_msec() - time_since_celebration > DURATION_CELEBRATION:
-		transition_state(GameManager.State.RESET)
+		transition_state(GameManager.State.RESET, state_data)
