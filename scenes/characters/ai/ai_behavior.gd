19550a7c51267648f12a600b4f7d39344b8f8e1b
ifarrell625 <ifarrell625@gmail.com>
2026-07-17 01:28:46 -0300

    added anti ball swarming steering force

--- scenes/characters/ai/ai_behavior.gd
+++ scenes/characters/ai/ai_behavior.gd
@@ -44,7 +44,6 @@ func get_bicircular_weight(position: Vector2, center_target: Vector2, inner_circ
 		
 
 
-
 func is_ball_possessed_by_opponent() -> bool:
 	return ball.carrier != null and ball.carrier.country != player.country
 
